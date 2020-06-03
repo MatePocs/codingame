1258098226b2197da2bc352a4fc50b092baca138
Mate Pocs <pocsmate1987@gmail.com>
2020-06-03 14:33:37 +0100

    phone number automatized

--- puzzle/phone_numbers.py
+++ puzzle/phone_numbers.py
@@ -4,46 +4,105 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-telephone_numbers = []
+class Node:
+    """
+    an object that has one job: keeping track of its children
+    """
+
+    def __init__(self):
+        self.children = {}
+        # children are a dictionary, key is an integer, value is a node
+
+    def add_child(self, key_of_child):
+        """
+        adds child to the dictionary of children
+        """
+        self.children[key_of_child] = Node()
+
+    def add_children(self, key_of_child_list):
+        """
+        adds children in a tree format
+        list is number of integers, goes deeper and deeper
+        """
+        current_node = self
+        for i in range(len(key_of_child_list)):
+            current_node.add_child(key_of_child_list[i])
+            current_node = current_node.children[key_of_child_list[i]]
+
+class PhoneBook:
+    """
+    main class, this will contain all the information
+    phone numbers as a tree and number of elements stored
+    """
+
+    def __init__(self):
+        self.number_of_elements = 0
+        self.phone_numbers = {}
+        # phone numbers are going to be a dictionary, key: integer, value: a node
+
+    def process_new_number(self, phone_number):
+        """
+        adds phone number to somewhere in the PhoneBook tree
+        phone_number is a string
+        """
+
+        numbers_list = self.convert_string_to_list_of_numbers(phone_number)
+
+        #check if first is in phone_numbers
+        if numbers_list[0] in self.phone_numbers:
+            # if yes, go in and search
+            current_node = self.phone_numbers[numbers_list[0]]
+            for i in range(1, len(numbers_list)):
+                # check if we can go one level deeper
+                if numbers_list[i] in current_node.children:
+                    # if it is in the current_node, great, move on
+                    current_node = current_node.children[numbers_list[i]]
+                    i += 1
+                else:
+                    # if not in the current node, need to add the remainder of 
+                    # the list to the current node
+                    current_node.add_children(numbers_list[i:])
+                    break
+
+            # and we know the number of new elements
+            new_elements = len(numbers_list) - i
+
+        else:
+            # if not, adds the first one, and then adds all the children
+            self.phone_numbers[numbers_list[0]] = Node()
+            self.phone_numbers[numbers_list[0]].add_children(numbers_list[1:])
+
+            new_elements = len(numbers_list)
+
+        # adds the number of new items stored to the number_of_elements
+        self.number_of_elements += new_elements
+
+    def convert_string_to_list_of_numbers(self, phone_number):
+        """
+        takes in a string, returns a list of integers
+        """
+        result_list = []
+
+        for char in phone_number:
+            result_list.append(int(char))
+
+        return result_list
+
+
+
+
+
+
+
+phonebook = PhoneBook()
 
 n = int(input())
 for i in range(n):
     telephone = input()
-    telephone_numbers.append(telephone)
-
-number_of_elements = 0
-number_of_elements += len(telephone_numbers[0])
-
-def is_number_sequence_in_list(number_sequence, list_of_numbers):
-    """
-    checks if there are any elements in the list_of_numbers that
-    start with the number_sequence
-    """
-    print(number_sequence, file = sys.stderr)
-    print(list_of_numbers, file = sys.stderr)
-    match = False
-    length_of_sequence = len(number_sequence)
-    for i in range(len(list_of_numbers)):
-        if list_of_numbers[i][:length_of_sequence] == number_sequence:
-            print(list_of_numbers[i][:length_of_sequence], file = sys.stderr)
-            match = True
-            break   
-    return match
+    phonebook.process_new_number(telephone)
 
 
-for i in range(1, len(telephone_numbers)):
-    current_number = telephone_numbers[i]
-    # for each length, check if there is another phone number already in the list 
-    # that starts with that sequence
-    # at the first length where we find no number: we need to store the rest
-    numer_of_characters_with_no_match = 0
-    for j in range(0, len(current_number)):
-        if is_number_sequence_in_list(current_number[:j+1],telephone_numbers[:i]):
-            pass
-        else:
-            numer_of_characters_with_no_match = len(current_number) - j
-            break
-    number_of_elements += numer_of_characters_with_no_match
+
 
 # The number of elements (referencing a number) stored in the structure.
-print(number_of_elements)
+print(phonebook.number_of_elements)
